88871625adca8668602355f429c1d323178f6334
hailun <zengboheng@gmail.com>
2021-12-25 20:25:04 +0800

    v1.0

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import Home from '../pages/Home'
+import Register from '../pages/Register'
+import Login from '../pages/Login'
+import Search from '../pages/Search'
+
+export default new VueRouter(
+    {
+        routes: [
+            {
+                path:'/home',
+                component:Home
+            },
+            {
+                path:'/register',
+                component:Register
+            },
+            {
+                path:'/login',
+                component:Login
+            },
+            {
+                name:"search",
+                path:'/search/:keyword',
+                component:Search
+            },
+            //重定向，在项目跑起来的时候，访问/，立马让他定向到首页
+            {
+                path:'*',
+                redirect:'/home'
+            }
+        ]
+    }
+)
+
