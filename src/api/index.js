59bb5db87f7559cbb59c7370f1227a1655509059
Zeng Hailun <zengboheng01@gmail.com>
2022-01-26 11:55:45 +0800

    "v.1.31"

--- src/api/index.js
+++ src/api/index.js
@@ -12,3 +12,7 @@ export const reqGetBannerList = ()=>mockRequests.get('/banner');
 
 //获取floor数据
 export const reqFloorList = ()=> mockRequests.get('/floor');
+
+//获取搜索模块数据，需要带参数
+//给服务器传递的默认参数至少要是一个空对象
+export const reqGetSearchInfo = (params)=>requests({url:"/list", method:"post",data:params})
--- src/store/search/index.js
+++ src/store/search/index.js
@@ -1,7 +1,36 @@
-const state = {}
-const mutations = {}
-const actions = {}//处理action，可以书写自己的业务逻辑，也可以处理异步
-const getters = {}//计算属性，用于简化仓库数据
+import { reqGetSearchInfo } from "../../api"
+
+const state = {
+    searchList:{}
+}
+const mutations = {
+    GETSEARCHLIST(state, searchList)
+    {
+        state.searchList = searchList
+    }
+}
+const actions = {
+    async getSearchList({commit}, params = {}){
+        let result = await reqGetSearchInfo(params)
+        if(result.code == 200)
+        {
+            commit("GETSEARCHLIST",result.data)
+        }
+    },
+}//处理action，可以书写自己的业务逻辑，也可以处理异步
+
+const getters = {
+    goodsList(state){
+        return state.searchList.goodsList||[];
+    },
+    trademarkList(state){
+        return state.searchList.trademarkList||[];
+    },
+    attrsList(state)
+    {
+        return state.searchList.attrsList||[];
+    }
+}//计算属性，用于简化仓库数据
 
 export default{
     state,
